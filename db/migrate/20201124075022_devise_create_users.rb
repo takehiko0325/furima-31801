f03387e23d66de2ae45d6c684598dcfbaec35117
takehiko0325 <73271123+takehiko0325@users.noreply.github.com>
2020-11-25 18:08:20 +0900

    userモデルの作成・カラム追加

--- db/migrate/20201124075022_devise_create_users.rb
+++ db/migrate/20201124075022_devise_create_users.rb
@@ -7,6 +7,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      t.string :name,null: false
+      t.string :first_name,null: false
+      t.string :family_name,null: false
+      t.string :first_name_kana,null: false
+      t.string :faimly_name_kana,null: false
+      t.date   :birthday,null: false
+
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
