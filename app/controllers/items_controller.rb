f74b173c205ee8435577474f74f530ea4207cc4c
takehiko0325 <73271123+takehiko0325@users.noreply.github.com>
2020-11-30 16:27:19 +0900

    パラムス

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   
   
   def index
-       @items = Item.all
+       @items = Item.includes(:user)
   end
 
   def new
@@ -11,8 +11,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
-
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -22,7 +21,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :image,:explanation , :category_id, :status_id,:fee_id, :delivery_source_region_id, :preparetion_day_id,:payment)
+    params.require(:item).permit(:name, :image,:explanation , :category_id, :status_id,:fee_id, :delivery_source_region_id, :preparetion_day_id,:payment).merge(user_id: current_user.id)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_075022) do
+ActiveRecord::Schema.define(version: 2020_11_24_024113) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -42,8 +42,10 @@ ActiveRecord::Schema.define(version: 2020_11_24_075022) do
     t.integer "delivery_source_region_id", null: false
     t.integer "preparetion_day_id", null: false
     t.integer "payment"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -59,4 +61,5 @@ ActiveRecord::Schema.define(version: 2020_11_24_075022) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "users"
 end
