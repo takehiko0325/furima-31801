a0463102de89e8fcbd1ac3f71501f7b94c8c5d6d
takehiko0325 <73271123+takehiko0325@users.noreply.github.com>
2020-12-03 21:44:18 +0900

    訂正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :show, :destroy ]
-  before_action :authenticate_user! ,only: [:new]
+  before_action :authenticate_user! ,only: [:new,:edit]
   def index
    @items = Item.includes(:user).order("created_at DESC")
   end
@@ -16,7 +16,9 @@ class ItemsController < ApplicationController
   end
 
   def  edit
-   
+    unless current_user == @item.user
+      redirect_to action: :show
+    end
   end
 
   def create
